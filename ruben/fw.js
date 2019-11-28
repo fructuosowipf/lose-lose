f12cd0173a1e2927482d266006f9993f09620c9d
fructuosowipf <ruben.fructuoso@wibilea.lokal>
2019-11-28 11:14:15 +0100

    jsonp -> json

--- ruben/fw.js
+++ ruben/fw.js
@@ -1,13 +1,13 @@
 /* Google Image Search API */
 // API-Key for this project: AIzaSyARcy2xM3pIK39g_O8nyhAo_htSDP8Le54
 
-let googleImgUrl = "https://www.googleapis.com/customsearch/v1?key=AIzaSyARcy2xM3pIK39g_O8nyhAo_htSDP8Le54&cx=001038306842663858513:0oetd1hoj58&searchType=image&fileType=jpg,png&q=fructuosowipf";
+let googleImgUrl = "https://www.googleapis.com/customsearch/v1?key=AIzaSyARcy2xM3pIK39g_O8nyhAo_htSDP8Le54&cx=001038306842663858513:0oetd1hoj58&searchType=image&fileType=jpg,png&q=berlin";
 let bild = [];
 
 $.ajax({
 
   url: googleImgUrl,
-  dataType: 'jsonp',
+  dataType: 'json',
   success: function(data) {
     googleImg = data.items;
 
