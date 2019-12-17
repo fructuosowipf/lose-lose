37dc0c1dcd827826cfda551b2602cc411b240d66
fructuosowipf <ruben.fructuoso@wibilea.lokal>
2019-12-17 11:04:57 +0100

    fernsehturm2

--- ruben/fw.js
+++ ruben/fw.js
@@ -9,6 +9,7 @@ let fernsehturm
 //Bilder vorladen
 function preload() {
   fernsehturm = loadImage('imgs/berlin_tit.png');
+  fernsehturm2 = loadImage('imgs/berlin_tit_2.png');
   /* Jedes weitere Bild muss hier unten eingefügt werden, hierzu Zeile oberhalb kopieren 
   und von obigen Variablen bedienen, bspw. für nächste Zeile: bild3 = loadImage('imgs/bild3.png'); -> Bild entsprechend unter /beat/imgs/ im github hochladen !!!
   AUSSERDEM: Für neues Bild ganz unten neue Einträge für die Rotation und für das Erzeugen des Bildes erstellen.
@@ -59,17 +60,17 @@ function draw() {
 
  //Objekt Bild erzeugen:
  rotate(random(5,300)); /* Rotation des Bildes, Angabe des Rotationswinkels in der Klammer */
- image(fernsehturm, random(0,windowWidth), random(0,windowHeight),random(30,1200),random(100,1800));
+ image(fernsehturm2, random(0,windowWidth), random(0,windowHeight),random(30,1200),random(100,1800));
  /* Erzeugt Bild, Werte: (Pfad zum Bild > wird über Variable "bildX" reingeladen wie oben beim Vorladen definiert, X-Koordinate, Y-Koordinate, Breite, Höhe) */
 
    //Objekt Bild erzeugen:
    rotate(random(5,300)); /* Rotation des Bildes, Angabe des Rotationswinkels in der Klammer */
-   image(fernsehturm, random(0,windowWidth), random(0,windowHeight),random(30,1200),random(100,1800));
+   image(fernsehturm2, random(0,windowWidth), random(0,windowHeight),random(30,1200),random(100,1800));
    /* Erzeugt Bild, Werte: (Pfad zum Bild > wird über Variable "bildX" reingeladen wie oben beim Vorladen definiert, X-Koordinate, Y-Koordinate, Breite, Höhe) */
 
      //Objekt Bild erzeugen:
  rotate(random(5,300)); /* Rotation des Bildes, Angabe des Rotationswinkels in der Klammer */
- image(fernsehturm, random(0,windowWidth), random(0,windowHeight),random(30,1200),random(100,1800));
+ image(fernsehturm2, random(0,windowWidth), random(0,windowHeight),random(30,1200),random(100,1800));
  /* Erzeugt Bild, Werte: (Pfad zum Bild > wird über Variable "bildX" reingeladen wie oben beim Vorladen definiert, X-Koordinate, Y-Koordinate, Breite, Höhe) */
   
 
