eb2a149b385ab46c9ab6e896e1fa34845d75575a
fructuosowipf <ruben.fructuoso@wibilea.ch>
2020-03-30 10:35:13 +0200

    logo again

--- ruben/battle.js
+++ ruben/battle.js
@@ -42,8 +42,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0); /* Hintergrundfarbe ändern https://p5js.org/reference/#/p5/background */
   frameRate(0.5) /* Anzahl Frames pro Sekunde in Klammer schreiben */;
-  h1 = createElement('h1', 'fructuoso/wipf');
-  h1. position(400,100);
+  /* h1 = createElement('h1', 'fructuoso/wipf');
+  h1. position(400,100); */
 }
 
 function draw() {
@@ -68,6 +68,7 @@ function draw() {
       //Objekt Bild erzeugen:
       rotate(random(0, 180)); /* Rotation des Bildes, Angabe des Rotationswinkels in der Klammer */
       image(battle1, 0, 0, windowWidth, windowHeight);
+      image(logo, windowWidth - 10, windowHeight - 10, 382, 56);
 
 
       break;
@@ -76,11 +77,15 @@ function draw() {
       rotate(random(0, 180)); /* Rotation des Bildes, Angabe des Rotationswinkels in der Klammer */
       image(battle2, 0, 0, windowWidth, windowHeight);
 
+      image(logo, windowWidth - 10, windowHeight - 10, 382, 56);
+
       break;
     case 3:
       //Objekt Bild erzeugen:
       rotate(random(0, 180)); /* Rotation des Bildes, Angabe des Rotationswinkels in der Klammer */
       image(battle3, 0, 0, windowWidth, windowHeight);
+
+      image(logo, windowWidth - 10, windowHeight - 10, 382, 56);
 
       break;
     case 4:
@@ -88,11 +93,15 @@ function draw() {
       rotate(random(0, 180)); /* Rotation des Bildes, Angabe des Rotationswinkels in der Klammer */
       image(battle4, 0, 0, windowWidth, windowHeight);
 
+      image(logo, windowWidth - 10, windowHeight - 10, 382, 56);
+
       break;
     case 5:
       //Objekt Bild erzeugen:
       rotate(random(0, 180)); /* Rotation des Bildes, Angabe des Rotationswinkels in der Klammer */
       image(battle5, 0, 0, windowWidth, windowHeight);
+
+      image(logo, windowWidth - 10, windowHeight - 10, 382, 56);
 
       break;
     case 6:
@@ -100,17 +109,23 @@ function draw() {
       rotate(random(0, 180)); /* Rotation des Bildes, Angabe des Rotationswinkels in der Klammer */
       image(battle6, 0, 0, windowWidth, windowHeight);
 
+      image(logo, windowWidth - 10, windowHeight - 10, 382, 56);
+
       break;
     case 7:
       //Objekt Bild erzeugen:
       rotate(random(0, 180)); /* Rotation des Bildes, Angabe des Rotationswinkels in der Klammer */
       image(battle7, 0, 0, windowWidth, windowHeight);
 
+      image(logo, windowWidth - 10, windowHeight - 10, 382, 56);
+
       break;
     case 8:
       //Objekt Bild erzeugen:
       rotate(random(0, 180)); /* Rotation des Bildes, Angabe des Rotationswinkels in der Klammer */
       image(battle8, 0, 0, windowWidth, windowHeight);
+
+      image(logo, windowWidth - 10, windowHeight - 10, 382, 56);
 
       break;
 
@@ -119,21 +134,20 @@ function draw() {
 
   }
 
-  image(logo, windowWidth-10, windowHeight-10, 382, 56);
 
   frameRate(random(0.1, 1)); /*Erzeugt nach dem Laden eines Objekts für das nächste Objekt eine neue Geschwindigkeit, dadurch wird eine unregelmässige Geschwindigkeit erzeugt. */
 
-/*   $(window).resize(function () {
-    createCanvas(windowWidth, windowHeight);
-    background(0);
-    redraw();
-  });
-
-  $(window).on('orientationchange', function () {
-    createCanvas(windowWidth, windowHeight);
-    background(0);
-    redraw();
-  }) */
+  /*   $(window).resize(function () {
+      createCanvas(windowWidth, windowHeight);
+      background(0);
+      redraw();
+    });
+  
+    $(window).on('orientationchange', function () {
+      createCanvas(windowWidth, windowHeight);
+      background(0);
+      redraw();
+    }) */
 
 }
 
@@ -143,27 +157,27 @@ function keyPressed() {
 
   if (keyCode === ENTER) {
 
-  let jetzt = new Date();
-  let tag = jetzt.getDate();
-  let monat = jetzt.getMonth() + 1;
-  let jahr = jetzt.getFullYear();
-  let stunden = jetzt.getHours();
-  let minuten = jetzt.getMinutes();
-  let sekunden = jetzt.getSeconds();
+    let jetzt = new Date();
+    let tag = jetzt.getDate();
+    let monat = jetzt.getMonth() + 1;
+    let jahr = jetzt.getFullYear();
+    let stunden = jetzt.getHours();
+    let minuten = jetzt.getMinutes();
+    let sekunden = jetzt.getSeconds();
 
 
 
 
-  let stempel = '_' + jahr + '_' + monat + '_' + tag + '_' + stunden + '_' + minuten + '_' + sekunden;
+    let stempel = '_' + jahr + '_' + monat + '_' + tag + '_' + stunden + '_' + minuten + '_' + sekunden;
 
-  save('fructuosowipf_loose_loose_loose_' + unique + stempel + '.jpg');
+    save('fructuosowipf_loose_loose_loose_' + unique + stempel + '.jpg');
 
-  return false;
+    return false;
 
-} else {
-  
-}
-  
+  } else {
+
+  }
+
 
 
 
